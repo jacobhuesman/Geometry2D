85a82d905e45c4809a8eeab72217ba6b2b817b09
Jacob Huesman <jacob.huesman@ndsu.edu>
2018-07-11 17:16:59 -0500

    Made format similar to StaticArrays

--- src/Point.jl
+++ src/Point.jl
@@ -1,37 +1,28 @@
-#   A point is defined by its two cartesian coordinate, put into a row
-#   vector of 2 elements:
-#   P = [x y];
-#
-#   Several points are stores in a matrix with two columns, one for the
-#   x-coordinate, one for the y-coordinate.
-#   PTS = [x1 y1 ; x2 y2 ; x3 y3];
-#
-#   Example
-#   P = [5 6];
+#   TODO add convert method
 
-struct Point{T<:Real}
-    A::Array{T,2}
+struct Point{T}
+    data::Array{T,2}
 
-    function Point{T}(A::Array{T}) where {T<:Real}
-        if size(A, 1) != 1 || size(A, 2) != 2
+    function Point{T}(data::Array{T}) where {T<:Real}
+        if size(data, 1) != 1 || size(data, 2) != 2
             error("Points have the form [x y]")
         else
-            new(A)
+            new(data)
         end
     end
 end
 
-Point(A::Array{T}) where {T<:Real} = Point{T}(A)
+Point(data::Array{T}) where {T<:Real} = Point{T}(data)
 Point(x::T, y::T) where {T<:Real} = Point{T}([x y])
 
-Base.:+(a::Point, b::Point) = Point(a.A + b.A)
-Base.:+(a::Array, b::Point) = Point(a   + b.A)
-Base.:+(a::Point, b::Array) = Point(a.A + b)
-Base.:-(a::Point, b::Point) = Point(a.A - b.A)
-Base.:-(a::Array, b::Point) = Point(a   - b.A)
-Base.:-(a::Point, b::Array) = Point(a.A - b)
+Base.:+(a::Point, b::Point) = Point(a.data + b.data)
+Base.:+(a::Array, b::Point) = Point(a      + b.data)
+Base.:+(a::Point, b::Array) = Point(a.data + b)
+Base.:-(a::Point, b::Point) = Point(a.data - b.data)
+Base.:-(a::Array, b::Point) = Point(a      - b.data)
+Base.:-(a::Point, b::Array) = Point(a.data - b)
 
-Base.getindex(a::Point, i) = a.A[i]
-Array(a::Point) = a.A
+Base.getindex(a::Point, i) = a.data[i]
+Array(a::Point) = a.data
 
-Base.show(io::IO, a::Point) = print(io, a.A, " (Point)")
+Base.show(io::IO, a::Point) = print(io, "Point:\n", a.data)
