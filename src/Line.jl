795e3ca1e51efe343d4b096e33c634f15ef3a1a1
Jacob Huesman <jacob.huesman@ndsu.edu>
2018-08-15 14:24:01 -0700

    Updated

--- src/Line.jl
+++ src/Line.jl
@@ -11,6 +11,7 @@ end
 
 Line(x1,y1,x2,y2) = Line(SVector{4,Float64}(x1, y1, x2, y2))
 Line(P1::SVector{2,Float64}, P2::SVector{2,Float64}) = Line(P1[1], P1[2], P2[1], P2[2])
+Line(p1::Point, p2::Point) = Line(p1[1], p1[2], p2[1], p2[2])
 Line(data::Array) = Line(data[1], data[2], data[3], data[4])
 Line(data1::Array, data2::Array) = Line(data1[1], data1[2], data2[1], data2[2])
 
@@ -19,7 +20,7 @@ convert(::Type{Line}, P::Array) = Line(P)
 Base.show(io::IO, a::Line) = print(io, "Line:\n", a.data)
 
 # TODO combine line implementations
-function draw(image::Array{T,2}, line::Line; value=0) where {T<:Real}
+function draw(image::Array{T,2}, line::Line; value=0) where {T<:Union{Real,Color}}
 	X = SVector{2,Float64}(line[1], line[2]);
 	Y = SVector{2,Float64}(line[3], line[4]);
 
@@ -36,7 +37,7 @@ function draw(image::Array{T,2}, line::Line; value=0) where {T<:Real}
 	end
 end
 
-function draw(image::Array{T,2}, line::Line, width::Real; value=0) where {T<:Real}
+function draw(image::Array{T,2}, line::Line, width::Real; value=0) where {T<:Union{Real,Color}}
 	X::SVector{2,Float64} = line[1:2];
 	Y::SVector{2,Float64} = line[3:4];
 	d::SVector{2,Float64} = Y - X;
--- src/Point.jl
+++ src/Point.jl
@@ -2,6 +2,7 @@
 # TODO switch to SVector
 # TODO switch to one dimensional array
 include("Geometry.jl")
+using Colors
 
 using StaticArrays
 
@@ -15,13 +16,13 @@ Point(data::Array{Float64,1}) = Point(data[1], data[2])
 Base.show(io::IO, a::Point) = print(io, "Point:\n", a.data)
 
 @enum PointType dot=1 cross=2
-function draw(image::Array{T,2}, point::Point; value::Int=0, pointtype::PointType=cross, size::Float64=4.0) where {T<:Real}
+function draw(image::Array{T,2}, point::Point; value=0, pointtype::PointType=cross, size::Float64=4.0, width::Float64=1.0) where {T<:Union{Real,Color}}
 	if (pointtype == dot)
 		return draw(image, Circle(point.data, size), value, true);
 	else
 		p = point.data;
 		s = size / 2;
-		draw(image, Line(p[1]-s, p[2]-s, p[1]+s, p[2]+s), value=value)
-		draw(image, Line(p[1]-s, p[2]+s, p[1]+s, p[2]-s), value=value)
+		draw(image, Line(p[1]-s, p[2]-s, p[1]+s, p[2]+s), width, value=value)
+		draw(image, Line(p[1]-s, p[2]+s, p[1]+s, p[2]-s), width, value=value)
 	end
 end
